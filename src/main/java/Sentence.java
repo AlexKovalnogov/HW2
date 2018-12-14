ae1e9d68899ae47cdfce577ebf527d9f6a4a892b
Oleksii Kovalnohov <oleksii.kovalnohov@worldapp.com>
2018-12-14 17:49:44 +0200

    Almost finishSentences

--- src/main/java/Sentence.java
+++ src/main/java/Sentence.java
@@ -4,49 +4,59 @@ import java.util.List;
 
 public class Sentence {
 
-    public void f(String Str) {
 
-        LinkedHashMap<Integer, String> integerStringLinkedHashMap = new LinkedHashMap<Integer, String>();
-        String sentenseAfterCuttingOddSpaces=cutOddSpacesInSentence(Str);
-        List<String> listOfWorlds = getWorldsFromSentence(sentenseAfterCuttingOddSpaces);
-        for (int i = 0; i < listOfWorlds.size() - 1; i++) {
-            integerStringLinkedHashMap.put(listOfWorlds.get(i).length(), listOfWorlds.get(i));
+
+
+    public void max(String Str) {
+
+        int maxCharacters = 0;
+        String world="";
+        String sentenceAfterCuttingOddSpaces = cutOddSpacesInSentence(Str);
+        List<String> listOfWorlds = getWorldsFromSentence(sentenceAfterCuttingOddSpaces);
+        for (int i = 0; i < listOfWorlds.size(); i++) {
+
+            if (listOfWorlds.get(i).length() > maxCharacters) {
+                maxCharacters = listOfWorlds.get(i).length() ;
+                world =listOfWorlds.get(i);
+            }
         }
+        System.out.println(maxCharacters+" "+world);
+
+    }
+
+    private String cutOddSpacesInSentence(String str) {
+
+        return str.trim().replaceAll("\\s{2,}", " ");
     }
 
     public List<String> getWorldsFromSentence(String Str) {
         List<Integer> arrayOfIndexes = getIndexesOfSpacesInSentence(Str);
-        List<String> listOfWorldsFromSentece = new ArrayList<String>();
+        List<String> listOfWorldsFromSentence = new ArrayList<String>();
         for (int i = 0; i < arrayOfIndexes.size() - 1; i++) {
             int beginIndex = arrayOfIndexes.get(i);
             int endIndex = arrayOfIndexes.get(i + 1);
-            listOfWorldsFromSentece.add(Str.substring(beginIndex, endIndex));
+            listOfWorldsFromSentence.add(Str.substring(beginIndex+1, endIndex));
         }
-        return listOfWorldsFromSentece;
+        return listOfWorldsFromSentence;
     }
 
-    private List<Integer> getIndexesOfSpacesInSentence(String senteceWithSpaces) {
+    private List<Integer> getIndexesOfSpacesInSentence(String sentenceWithSpaces) {
 
         int UNICODEOFSPACE = 32;
         List<Integer> arrayIndexesOfSpaces = new ArrayList<Integer>();
-        for (int i = 0; i <= senteceWithSpaces.length() - 1; i++) {
-            if (senteceWithSpaces.charAt(i) == UNICODEOFSPACE) {
+        for (int i = 0; i <= sentenceWithSpaces.length() - 1; i++) {
+            if (sentenceWithSpaces.charAt(i) == UNICODEOFSPACE) {
                 arrayIndexesOfSpaces.add(i);
             }
         }
-        arrayIndexesOfSpaces.add(0, 0);
-        arrayIndexesOfSpaces.add(senteceWithSpaces.length());
+        arrayIndexesOfSpaces.add(0, -1);
+        arrayIndexesOfSpaces.add(sentenceWithSpaces.length());
         return arrayIndexesOfSpaces;
     }
 
-private  String cutOddSpacesInSentence(String str) {
-
-return   str.trim().replaceAll("\\s{2,}", " ");
-}
-
 
     public static void main(String[] args) {
-        new Sentence().f("   test1   test12   test123   Test1234");
+        new Sentence().f("   test1   test12   test123   Test1234   htntyntyntynyntntnytynytn ");
         // Sentence sentence = new Sentence();
           /*  String s=" test1 test12 test123";
 
